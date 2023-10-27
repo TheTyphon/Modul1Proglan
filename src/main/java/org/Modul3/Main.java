943fb9732a22851b60a9042fa6a7780daaea3f0a
TheTyphon <facebookwingchun@gmail.com>
2023-10-27 20:09:04 +0700

    Menambahkan line print baru

--- src/main/java/org/Modul3/Main.java
+++ src/main/java/org/Modul3/Main.java
@@ -4,6 +4,9 @@ package org.Modul3;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello, Git!");
+        System.out.println("Ini setelah update.");
     }
+
 }
